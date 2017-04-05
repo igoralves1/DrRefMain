35980a8d9602b595f6b1c545065acae38d8b5caf
igoralves1 <igoralves1@gmail.com>
2017-04-05 13:39:40 -0400

    After Add Debug Bar - All is working

--- config/app.php
+++ config/app.php
@@ -176,6 +176,11 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        
+        /*
+         * Custom
+         */
+        Barryvdh\Debugbar\ServiceProvider::class,
 
     ],
 
@@ -225,6 +230,11 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        
+        /*
+         * Custom
+         */
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
 
     ],
 
