915d7c7622b23f3ff7b3aee98f8b301f0b7c3324
Igor Alves <igoralves1@gmail.com>
2018-01-16 23:52:32 -0500

    jan2018

--- app/Http/Controllers/SitePublic/HomeController.php
+++ app/Http/Controllers/SitePublic/HomeController.php
@@ -8,7 +8,20 @@ use Illuminate\Support\Facades\App;
 
 class HomeController extends Controller
 {
-    public function index($account,$location) {
+    public function index(Request $req,$account,$location) {
+        
+        
+//   dd(Route::current());		
+//   $currentPath= "uuu";		
+//   $currentPath= Route::getFacadeRoot()->current()->uri();		
+//   $currentPath= Route::getFacadeRoot();		
+//   $currentPath= Route::pathInfo();		
+//   dd($currentPath);		
+//   $uri = Illuminate\Http\Request::path();		
+   //$req=Illuminate\Http\Request;		
+   //dd($req->path());
+   dd($req->path());
+      
         
         $loc=explode("_", $location);
         $locale = $loc[0];
